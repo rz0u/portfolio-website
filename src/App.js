26d70ff207fc31727799ee21ee4878b61a56a3c0
rz0u <rayhan.zou98@gmail.com>
2024-01-29 11:17:07 +0700

    feat: template components and template dynamic data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import styled, { ThemeProvider } from "styled-components";
+import { darkTheme } from "./utils/themes";
+
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  width: 100%;
+  overflow-x: hidden;
+`;
 
 function App() {
-  return <div className="App">Portfolio</div>;
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Body>Portfolio</Body>
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/utils/themes.js
@@ -0,0 +1,22 @@
+export const darkTheme = {
+  bg: "#1c1c27",
+  bgLight: "#1c1e27",
+  primary: "#854ce6",
+  textPrimary: "#f2f3f4",
+  textSecondary: "#b1b2b3",
+  card: "#171721",
+  cardLight: "#191924",
+  button: "#854ce6",
+  white: "#fff",
+  black: "#000",
+};
+
+export const lightTheme = {
+  bg: "#fff",
+  bgLight: "#f0f0f0",
+  primary: "#beladb",
+  textPrimary: "#111",
+  textSecondary: "#48494a",
+  card: "#fff",
+  button: "#5c5b5b",
+};
